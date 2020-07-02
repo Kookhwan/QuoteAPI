446df108dc63eeb9197451e4cf2fb76b50e5bc8a
Kookhwan Im <tatata1358@gmail.com>
2020-07-02 11:40:07 -0400

    create classes for Purchase

---
+++ QuoteAPI/Models/PurchaseOrder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class PurchaseOrder
+    {
+        public int ProjectId { get; set; }
+        public int OrderId { get; set; }
+        public int SupplierId { get; set; }
+        public string ShippingPlace { get; set; }
+        public string ShippingAddress { get; set; }
+        public string ShippingPhone { get; set; }
+        public DateTime? EstimatedShipdate { get; set; }
+        public int? OrderedBy { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+    }
+}
---
+++ QuoteAPI/Models/PurchaseStock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class PurchaseStock
+    {
+        public int ProjectId { get; set; }
+        public int OrderId { get; set; }
+        public string ProductId { get; set; }
+        public string Dimension { get; set; }
+        public string Description { get; set; }
+        public int? Pattern { get; set; }
+        public int? Colour { get; set; }
+        public decimal? Quantity { get; set; }
+        public string Uom { get; set; }
+        public decimal? UnitPrice { get; set; }
+        public decimal? Amount { get; set; }
+        public int? ReceivedBy { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+    }
+}
