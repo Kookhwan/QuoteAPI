3fdce8237be8548b5c2135ce8ca6e73d60e7e3a1
Kookhwan Im <tatata1358@gmail.com>
2020-07-02 11:42:51 -0400

    create a class for Invoice

---
+++ QuoteAPI/Models/Invoice.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class Invoice
+    {
+        public int ProjectId { get; set; }
+        public DateTime InvoiceDate { get; set; }
+        public int? Terms { get; set; }
+        public string BillPlace { get; set; }
+        public string BillAddress { get; set; }
+        public string BillPhone { get; set; }
+        public string BillEmail { get; set; }
+        public string ShipPlace { get; set; }
+        public string ShipAddress { get; set; }
+        public decimal? Subtotal { get; set; }
+        public decimal? Shipping { get; set; }
+        public decimal? Hst { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public int? InvoiceBy { get; set; }
+        public DateTime? CreatedTime { get; set; }
+        public int? UpdatedBy { get; set; }
+        public DateTime? UpdatedTime { get; set; }
+    }
+}
