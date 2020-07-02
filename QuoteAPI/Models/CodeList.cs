115b0f8c0be004c9b6a6a5d9ca58b65e8a022a54
Kookhwan Im <tatata1358@gmail.com>
2020-07-02 11:20:02 -0400

    create classes for CodeLists

---
+++ QuoteAPI/Models/CodeList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class CodeList
+    {
+        public int CategoryId { get; set; }
+        public int CodeId { get; set; }
+        public string CodeName { get; set; }
+    }
+}
