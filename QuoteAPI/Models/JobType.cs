40daae303a1582d6bebf5763c3871917dc921b90
Kookhwan Im <tatata1358@gmail.com>
2020-07-02 11:30:41 -0400

    create a class for Job Types

---
+++ QuoteAPI/Models/JobType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class JobType
+    {
+        public int JobId { get; set; }
+        public string JobTitle { get; set; }
+    }
+}
