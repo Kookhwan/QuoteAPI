bfa1d63cd12fab59d46df61aaa0e4c5bb76262a9
Kookhwan Im <tatata1358@gmail.com>
2020-07-02 00:11:45 -0400

    create a class for Employee

---
+++ QuoteAPI/Models/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuoteAPI.Models
+{
+    public class Employee
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Password { get; set; }
+        public int JobId { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string Postalcode { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
